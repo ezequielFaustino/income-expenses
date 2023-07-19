43a40c179b9bdf2e56e2188149e4b1e4cfa656aa
Ezequiel Faustino <ezequielfaustino@gmail.com>
2023-07-19 19:21:20 -0400

    iniciado estrutura de pastas da aplicação

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const postcssPresetEnv = require('postcss-preset-env')
+const tailwindcss = require('tailwindcss')
+const autoprefixer = require('autoprefixer')
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    postcssPresetEnv({stage: 2}),
+    require('cssnano'),
+    // tailwindcss({}),
+    // autoprefixer({}),
+  ]
+}
