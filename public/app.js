38742a608d8706a3c51822492e68380e775e47e7
Ezequiel Faustino <ezequielfaustino@gmail.com>
2023-07-22 19:59:13 -0400

    adicionado lista de transacoes ao a página

--- public/app.js
+++ public/app.js
@@ -1,53 +1,49 @@
 const balance = document.querySelector('[data-js="balance"]')
-
 const moneyPlus = document.querySelector('[data-js="money-plus"]')
 const moneyMinus = document.querySelector('[data-js="money-minus"]')
 
 const form = document.querySelector('[data-js="form"]')
 const addBtn = document.querySelector('[data-js="add-btn"]')
 
+const transactionsList = [
+  {id: 1, name: 'Aluguel', amount: -300},
+  {id: 2, name: 'Salário', amount: 900},
+  {id: 3, name: 'Alimentação', amount: 300}
+]
+
+const addTransactionIntoDom = (transaction) => {
+  const li = document.createElement('li')
+  const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
+  const operator = transaction.amount > 0 ? '' : '-'
+  const amountWithoutOperator = Math.abs(transaction.amount)
+  li.classList.add(CSSClass)
+  li.classList.add('border-b-2')
+  li.classList.add('border-stone-400')
+  
+
+  li.innerHTML = `${transaction.name}: R$ ${operator}${amountWithoutOperator}`
+
+  const fragment = document.createDocumentFragment()
+  const transactionsUl = document.querySelector('[data-js="transactions-list"]')
+
+
+  fragment.append(li)
+  transactionsUl.prepend(fragment)
+  
+}
+
+
+const init = transactionsList.forEach(addTransactionIntoDom);
+
 const dontRefreshPage = event => event.preventDefault()
 
-
-const getTransactionsList = (amount) => {
-  const transactionsList = []
-
-  if (amount > 0) {
-    transactionsList.push({ income: amount })
-  } else if (amount < 0) {
-    transactionsList.push({ expense: amount })
-  }
-
-  return transactionsList
-}
-
-const incomes = amount => {
-  if (amount > 0) {
-    transactionsList.map(({ income }) => {
-      return `${income}`
-    })
-
-  }
-}
-
-const expenses = amount => {
-  if (amount < 0) {
-    transactionsList.map(({ expense }) => {
-      return `${expense}`
-    })
-  }
-}
 
 const clickEvent = (e) => {
   addBtn.onpointerenter = addBtn
   addBtn.setPointerCapture(e.pointerId)
 
   const amount = document.querySelector('[data-js="amount"]').value
-  
-  const inOrOut = getTransactionsList(+amount)
-  console.log(inOrOut)
 }
-
 
 form.addEventListener('submit', dontRefreshPage)
 addBtn.onpointerdown = clickEvent
