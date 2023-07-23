cdbadef849b1efd218816f92df9a99624e14d677
Ezequiel Faustino <ezequielfaustino@gmail.com>
2023-07-22 21:04:29 -0400

    somareceitas e despesas e add ao saldo atual

--- public/app.js
+++ public/app.js
@@ -6,10 +6,13 @@ const form = document.querySelector('[data-js="form"]')
 const addBtn = document.querySelector('[data-js="add-btn"]')
 
 const transactionsList = [
-  {id: 1, name: 'Aluguel', amount: -300},
-  {id: 2, name: 'Salário', amount: 900},
-  {id: 3, name: 'Alimentação', amount: 300}
+  { id: 1, name: 'Aluguel', amount: -400 },
+  { id: 2, name: 'Salário', amount: 900 },
+  { id: 3, name: 'Placa de vídeo', amount: 500 },
+  { id: 4, name: 'Alimentação', amount: -300 }
 ]
+
+const dontRefreshPage = event => event.preventDefault()
 
 const addTransactionIntoDom = (transaction) => {
   const li = document.createElement('li')
@@ -19,31 +22,46 @@ const addTransactionIntoDom = (transaction) => {
   li.classList.add(CSSClass)
   li.classList.add('border-b-2')
   li.classList.add('border-stone-400')
-  
 
   li.innerHTML = `${transaction.name}: R$ ${operator}${amountWithoutOperator}`
 
   const fragment = document.createDocumentFragment()
   const transactionsUl = document.querySelector('[data-js="transactions-list"]')
 
-
   fragment.append(li)
-  transactionsUl.prepend(fragment)
-  
+  transactionsUl.append(fragment)
+
+  const total = sumBalanceValues()
+  balance.innerText = `R$${total}.00`
+
+}
+
+const getBalanceValues = () => {
+  const getAmount = transactionsList.map(({amount}) => amount)
+  return getAmount
+}
+
+const sumBalanceValues = () => {
+ const getAmount =  getBalanceValues()
+ const sum = getAmount.reduce((accumulator, amount) => accumulator + amount, 0,)
+
+ return sum
 }
 
 
-const init = transactionsList.forEach(addTransactionIntoDom);
-
-const dontRefreshPage = event => event.preventDefault()
-
+const init = () => {
+  transactionsList.forEach(addTransactionIntoDom)
+  sumBalanceValues()
+}
 
 const clickEvent = (e) => {
   addBtn.onpointerenter = addBtn
   addBtn.setPointerCapture(e.pointerId)
 
   const amount = document.querySelector('[data-js="amount"]').value
+  const transactionName = document.querySelector('[data-js="text"]').value
 }
 
 form.addEventListener('submit', dontRefreshPage)
 addBtn.onpointerdown = clickEvent
+init()
