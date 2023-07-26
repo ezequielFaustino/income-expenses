189bc978965d7ede52631927047420f646e09db4
Ezequiel Faustino <ezequielfaustino@gmail.com>
2023-07-25 20:19:13 -0400

    iniciado função para add receitas e despesas

--- public/app.js
+++ public/app.js
@@ -5,17 +5,14 @@ const moneyMinus = document.querySelector('[data-js="money-minus"]')
 const form = document.querySelector('[data-js="form"]')
 const addBtn = document.querySelector('[data-js="add-btn"]')
 
-const transactionsList = [
-  { id: 1, name: 'Aluguel', amount: -400 },
-  { id: 2, name: 'Salário', amount: 900 },
-  { id: 3, name: 'Placa de vídeo', amount: 500 },
-  { id: 4, name: 'Alimentação', amount: -300 }
-]
+const transactionsList = [{}]
 
 const dontRefreshPage = event => event.preventDefault()
 
 const addTransactionIntoDom = (transaction) => {
   const li = document.createElement('li')
+  addTransactionIntoList(transaction)
+  
   const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
   const operator = transaction.amount > 0 ? '' : '-'
   const amountWithoutOperator = Math.abs(transaction.amount)
@@ -35,46 +32,59 @@ const addTransactionIntoDom = (transaction) => {
   balance.innerText = `R$${total}.00`
 }
 
+console.log(transactionsList)
+
 const getBalanceValues = () => {
-  const getAmount = transactionsList.map(({amount}) => amount)
+  const getAmount = transactionsList.map(({ amount }) => amount)
   return getAmount
 }
 
 const sumBalanceValues = () => {
- const getAmount =  getBalanceValues()
- const sum = getAmount.reduce((accumulator, amount) => accumulator + amount, 0,)
+  const getAmount = getBalanceValues()
+  const sum = getAmount.reduce((accumulator, amount) => accumulator + amount, 0,)
 
- return sum
+  return sum
 }
 
 const incomesAndExpenses = () => {
   const getAmount = getBalanceValues()
-  
+
   const incomes = getAmount.filter((amount) => amount > 0)
     .reduce((accumulator, amount) => accumulator + amount, 0)
 
   const expenses = getAmount.filter((amount) => amount < 0)
     .reduce((accumulator, amount) => accumulator + amount, 0)
-  
+
   moneyPlus.innerText = `R$ ${incomes}.00`
   moneyMinus.innerText = `R$ ${expenses}.00`
 
 }
 
 
+const addTransactionIntoList = (transaction) => {
+  const transactionName = document.querySelector('[data-js="text"]').value
+  const amountValue = document.querySelector('[data-js="amount"]').value
+
+  const name = transaction.name = transactionName
+  const amount = transaction.amount = amountValue
+  const newObj = [{ name, amount}]
+
+  return newObj
+}
+
+
 const init = () => {
   transactionsList.forEach(addTransactionIntoDom)
   incomesAndExpenses()
+  
 }
 
 const clickEvent = (e) => {
   addBtn.onpointerenter = addBtn
   addBtn.setPointerCapture(e.pointerId)
 
-  const amount = document.querySelector('[data-js="amount"]').value
-  const transactionName = document.querySelector('[data-js="text"]').value
+  init()
 }
 
 form.addEventListener('submit', dontRefreshPage)
 addBtn.onpointerdown = clickEvent
-init()
