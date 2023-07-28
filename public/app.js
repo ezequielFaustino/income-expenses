f04591fb6ab95d6998b26bc9af724c4afcc13f7b
Ezequiel Faustino <ezequielfaustino@gmail.com>
2023-07-28 19:44:47 -0400

    refatorando aplicação

--- public/app.js
+++ public/app.js
@@ -1,22 +1,17 @@
-import * as localStorage from './localStorage.js'
-
-const balance = document.querySelector('[data-js="balance"]')
-const moneyPlus = document.querySelector('[data-js="money-plus"]')
-const moneyMinus = document.querySelector('[data-js="money-minus"]')
-
+const balanceDisplay = document.querySelector('[data-js="balance"]')
+const moneyPlusDisplay = document.querySelector('[data-js="money-plus"]')
+const moneyMinusDisplay = document.querySelector('[data-js="money-minus"]')
 const form = document.querySelector('[data-js="form"]')
+const transactionInput = document.querySelector('[data-js="text"]')
+const amountInput = document.querySelector('[data-js="amount"]')
+const transactionsUl = document.querySelector('[data-js="transactions-list"]')
 const addBtn = document.querySelector('[data-js="add-btn"]')
 
-const dontRefreshPage = event => event.preventDefault()
+let transactions = []
 
-const newList = localStorage.list
-localStorage.setLocalStorage(newList)
-const getLocalStorage = localStorage.getLocalStorage()
-
-
-const addTransactionIntoDom = (transaction) => {
+/*
+const addTransactionIntoDom = ({amount, name, id}) => {
   const li = document.createElement('li')
-  // addTransactionIntoList(transaction)
   const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
   const operator = transaction.amount > 0 ? '' : '-'
   const amountWithoutOperator = Math.abs(transaction.amount)
@@ -27,53 +22,57 @@ const addTransactionIntoDom = (transaction) => {
   li.innerHTML = `${transaction.name}: R$ ${operator}${amountWithoutOperator}`
 
   const fragment = document.createDocumentFragment()
-  const transactionsUl = document.querySelector('[data-js="transactions-list"]')
-
   fragment.append(li)
   transactionsUl.append(fragment)
-
 }
+*/
 
+/*
 const updateBalanceValues = () => {
-  const transactionsAmounts = getLocalStorage.map(({amount}) => amount)
+  const getLocalStorage = localStorage.getLocalStorage()
+
+  const transactionsAmounts = getLocalStorage.map( ({amount}) => amount)
+  console.log(transactionsAmounts)
   const total = transactionsAmounts
-    .reduce((accumulator, amount) => accumulator + amount, 0).toFixed(2)
-  balance.innerText = `R$${total}`
-  
+    .reduce((accumulator, amount) => accumulator + amount, 0)
+
+
   const income = transactionsAmounts
     .filter(value => value > 0)
-    .reduce((accumulator, amount) => accumulator + amount, 0)
-  moneyPlus.innerText = `R$ ${income.toFixed(2)}`
-  
+    .reduce((accumulator, value) => accumulator + value, 0)
+
   const expense = transactionsAmounts
     .filter(value => value < 0)
-    .reduce((accumulator, amount) => accumulator + amount, 0)
-  moneyMinus.innerText = `R$ ${expense.toFixed(2)}`
+    .reduce((accumulator, value) => accumulator + value, 0)
+
+  balanceDisplay.textContent = `R$${total}`
+  moneyMinusDisplay.textContent = `R$ ${expense}`
+  moneyPlusDisplay.textContent = `R$ ${income}`
 }
-
-const addTransactionIntoList = (transaction) => {
-  const transactionName = document.querySelector('[data-js="text"]').value
-  const amountValue = document.querySelector('[data-js="amount"]').value
-
-  const name = transaction.name = transactionName
-  const amount = transaction.amount = amountValue
-  const newObj = [{ name, amount }]
-
-  return newObj
-}
-
+*/
 const init = () => {
-  getLocalStorage.forEach(addTransactionIntoDom)
-  updateBalanceValues()
-}
-
-init()
-
-const clickEvent = (e) => {
-  addBtn.onpointerenter = addBtn
-  addBtn.setPointerCapture(e.pointerId)
 
 }
 
-form.addEventListener('submit', dontRefreshPage)
-addBtn.onpointerdown = clickEvent
+const generateID = () => Math.round(Math.random() * 1000)
+
+const addTransactionsArray = (transactionName, transactionAmount) => {
+  transactions.push({
+    id: generateID(),
+    name: transactionName,
+    amount: Number(transactionAmount)
+  })
+
+  console.log(transactions)
+}
+
+const handleFormSubmit = event => {
+  event.preventDefault()
+  
+  const transactionName = transactionInput.value.trim()
+  const transactionAmount = amountInput.value.trim()
+  
+  addTransactionsArray(transactionName, transactionAmount)
+}
+
+form.addEventListener('submit', handleFormSubmit)
--- public/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const getLocalStorage =  () => JSON.parse(localStorage.getItem('db')) ?? []
-
-export const setLocalStorage = db => localStorage.setItem('db', JSON.stringify(db))
-
-export const list = [
-  {name: 'Salário', amount: 2000},
-  {name: 'Aluguel', amount: -600}
-]
